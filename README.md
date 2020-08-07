44e5ad7c8d720cfb539ef6a9cca6a68d44c12baf
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-08-06 19:00:04 -0600

    first init

---
+++ README.md
@@ -0,0 +1,22 @@
+﻿
+# Pasta Disasta
+
+## About
+
+A meme website mocking lawyers
+
+
+
+## Lib
+
+* [matter.js](https://brm.io/matter-js/)
+* 
+
+
+
+## Licence
+
+The rules for copy and distributing this project licence are
+outlined in the licence.txt file.
+
+This project is under an MIT licence
---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+// from https://github.com/liabru/matter-js/wiki/Getting-started
+// module aliases
+let Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies
+
+// create an engine
+let engine = Engine.create()
+
+
+// create a renderer
+let render = Render.create({
+    element: document.body,
+    engine: engine
+})
+
+// create two boxes and a ground
+let boxA = Bodies.rectangle(400, 200, 80, 80)
+let boxB = Bodies.rectangle(450, 50, 80, 80)
+
+// static body
+let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
+
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground])
+
+// run the engine
+Engine.run(engine)
+
+// run the renderer
+Render.run(render)
