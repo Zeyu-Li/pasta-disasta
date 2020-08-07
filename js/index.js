323744d8dac33b8100ba2063611f85ff79e12734
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-08-06 20:37:01 -0600

    matter working

--- js/index.js
+++ js/index.js
@@ -4,30 +4,76 @@
 let Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
+    Svg = Matter.Svg,
     Bodies = Matter.Bodies
 
-// create an engine
 let engine = Engine.create()
 
 
 // create a renderer
 let render = Render.create({
     element: document.body,
-    engine: engine
+    engine: engine,
+    options: {
+        width: 1500,
+        height: 700,
+        pixelRatio: 1,
+        background: '#111',
+        wireframeBackground: '#222',
+        hasBounds: false,
+        enabled: true,
+        wireframes: true,
+        showSleeping: true,
+        showDebug: false,
+        showBroadphase: false,
+        showBounds: false,
+        showVelocity: true,
+        showCollisions: false,
+        showSeparations: false,
+        showAxes: false,
+        showPositions: false,
+        showAngleIndicator: false,
+        showIds: false,
+        showShadows: false,
+        showVertexNumbers: false,
+        showConvexHulls: false,
+        showInternalEdges: false,
+        showMousePosition: false
+    }
 })
 
 // create two boxes and a ground
-let boxA = Bodies.rectangle(400, 200, 80, 80)
-let boxB = Bodies.rectangle(450, 50, 80, 80)
+let circle1 = Bodies.circle(100, 50, 80, { density: 1 })
+// let circle2 = Bodies.circle(400, 400, 40)
+
+// svg
+$('#sticky').find('path').each(function(i, path){
+    // vertexSets.push(Svg.pathToVertices(path, 100));
+    var v = Bodies.fromVertices(100+(i*80), 80, Svg.pathToVertices(path, 20), {
+        render: {
+            fillStyle: '#556270',
+            strokeStyle: '#556270'
+        }
+    }, true);
+    vertexSets.push(v);
+  // World.add(engine.world, v);
+});
 
 // static body
 let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
+// ramp
+let ground2 = Bodies.rectangle(300, 500, 810, 60, { isStatic: true, angle: Math.PI * 0.2})
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground])
+World.add(engine.world, [circle1, ground, ground2])
 
 // run the engine
 Engine.run(engine)
 
 // run the renderer
 Render.run(render)
+
+// resets page
+$('.reset').click(()=>{
+    location.reload()
+})
